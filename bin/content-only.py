9b17bd28bf7bf62365d2fd75f208346e31bc41d1
Chris <chris_pw@gmx.de>
2025-11-26 20:21:19 +0100

    add lxml features flag

--- bin/content-only.py
+++ bin/content-only.py
@@ -3,7 +3,7 @@ import sys
 
 directory = sys.argv[1]
 
-main = bs4.BeautifulSoup(open(f'{directory}/index.html', 'rb')).find('article')
+main = bs4.BeautifulSoup(open(f'{directory}/index.html', 'rb'), features='lxml').find('article')
 
 with open(f'{directory}/faq.content-only.html', 'wb+') as fp:
     fp.write(bytes(str(main), 'utf-8'))
